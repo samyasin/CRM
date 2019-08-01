4a5930029692464144ebf2b1782d636f2cb4b621
Noor Aldeen <nooralazzeh97@gmail.com>
2019-08-01 13:11:46 +0300

    includes folder

---
+++ includes/connect_db.php
@@ -0,0 +1,12 @@
+<?php
+//$con = mysqli_connect('pdb25.runhosting.com','2719221_upskillsnew','6attYNxxRZije4s','2719221_upskillsnew');
+$con = mysqli_connect('localhost','root','0000','upskillsnew');
+    if(!($con)){
+        
+        echo "something Wrong";
+    }
+// one of two command enough.
+//mysqli_query($con, "SET NAMES 'utf8'"); 
+mysqli_query($con, "SET CHARACTER SET utf8"); 
+
+?>
